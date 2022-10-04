dd0da6a57f9ca28569bffc3a4428303ea899dc1d
hanbinChae <hanbin2043@gmail.com>
2022-10-04 14:30:11 +0900

    10/4

--- 컴퓨터네트워크/Client.py
+++ 컴퓨터네트워크/Client.py
@@ -7,20 +7,19 @@ Created on Tue Oct  4 11:22:42 2022
 """
 
 import socket
-IP = '10.2.20.214'
+IP = '192.168.46.218'
 PORT = 9999
 address = (IP,PORT)
 
 client = socket.socket()
-
 client.connect(address)
 print('1. ----- Server is connected -----')
-
-client.send(input().encode()) # encode 문자->바이트
-print('2. ----- Message send -----')
-
-rxmsg = client.recv(1024)
-print('3. ----- message recieved -----')
-print("RX:",rxmsg.decode())
-
+while True:
+    client.send(input().encode()) # encode 문자->바이트
+    print('2. ----- Message send -----')
+    
+    rxmsg = client.recv(1024)
+    print('3. ----- message recieved -----')
+    print("RX:",rxmsg.decode())
+    
 client.close()
